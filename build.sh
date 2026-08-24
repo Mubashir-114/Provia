55f85b63a61515bd68f2d6722a42c5c8b3b13856
mubashir <mklmubashir575@gmail.com>
2026-08-24 14:34:15 +0530

    diagnose Render Django staticfiles

--- build.sh
+++ build.sh
@@ -6,4 +6,12 @@ pip install -r requirements.txt
 npm install
 npm run build:css
 
-python manage.py collectstatic --noinput
+
+echo "=== DJANGO DIAGNOSTIC ==="
+python --version
+python manage.py version
+python manage.py help | grep collectstatic || true
+python manage.py shell -c "from django.conf import settings; print('SETTINGS FILE:', settings.SETTINGS_MODULE); print('STATICFILES INSTALLED:', 'django.contrib.staticfiles' in settings.INSTALLED_APPS)"
+echo "=== END DIAGNOSTIC ==="
+
+python manage.py collectstatic --no-input
