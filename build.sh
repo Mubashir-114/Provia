8a95f639f02a13ada040463f14aaf51a83f13c20
mubashir <mklmubashir575@gmail.com>
2026-08-24 14:37:41 +0530

    inspect Render settings module

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ npm run build:css
 echo "=== DJANGO DIAGNOSTIC ==="
 python --version
 python manage.py version
-python manage.py help | grep collectstatic || true
-python manage.py shell -c "from django.conf import settings; print('SETTINGS FILE:', settings.SETTINGS_MODULE); print('STATICFILES INSTALLED:', 'django.contrib.staticfiles' in settings.INSTALLED_APPS)"
+python manage.py shell -c "import config.settings; print('SETTINGS MODULE:', config.settings.__file__); from django.conf import settings; print('SETTINGS FILE:', settings.SETTINGS_MODULE); print('STATICFILES INSTALLED:', 'django.contrib.staticfiles' in settings.INSTALLED_APPS); print('INSTALLED APPS:', settings.INSTALLED_APPS)"
 echo "=== END DIAGNOSTIC ==="
 
 python manage.py collectstatic --no-input
